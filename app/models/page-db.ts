63a773af37eea534778b89d44b7d030c66f1bb42
Viktor Ödman <vo222dq@student.lnu.se>
2021-12-07 21:56:42 +0100

    Working on query

--- app/models/page-db.ts
+++ app/models/page-db.ts
@@ -1,11 +1,14 @@
 import Page from "./page";
 
 export default class PageDB {
-    private _wordTold: Map<string, number> = new Map<string,number>();
+    private _wordTold: Map<string, number> = new Map<string, number>();
     private _pages: Page[] = [];
 
+    public set pages(newPages: Page[]) {
+        this._pages = newPages;
+    }
     
-    public getPagesWithWord(word: string) {
+    public getPagesNamesWithWord(word: string): string[] {
         const pagesWithWord: string[] = [];
         
         if (this._wordTold.has(word)) {
@@ -21,13 +24,27 @@ export default class PageDB {
         return pagesWithWord;
     }
 
+    public getPagesWithWord(word: string): Page[] {
+        const pagesIncludingWord: Page[] = [];
+
+        if (this.includesWord(word)) {
+            const wordIndex = this.getIdForWord(word);
+
+            for (const page of this._pages) {
+                if (page.containsWord(wordIndex)) {
+                    pagesIncludingWord.push(page);
+                }
+            }
+        }
+
+        return pagesIncludingWord
+    }
+
     public includesWord(word: string): boolean {
         return (this._wordTold.has(word)); 
     }
 
     public addPage(page: Page, wordsFromFile: string[]): void {
-        const wordIds: number[] = []
-
         for (const word of wordsFromFile) {
             const wordID = this.getIdForWord(word);
             page.addWord(wordID)
--- app/services/search-service.ts
+++ app/services/search-service.ts
@@ -10,7 +10,9 @@ export default class SearchService {
     public async searchWord(word: string):Promise<Score[]> {
         const pageDB: PageDB = await this.createPageDB();
 
-        /* const pagesWithWord = pageDB.getPagesWithWord(word); */
+        const pagesWithWord = pageDB.getPagesWithWord(word);
+
+        pageDB.pages = pagesWithWord;
 
         const result: Score[] = this.query(pageDB, word); 
 
